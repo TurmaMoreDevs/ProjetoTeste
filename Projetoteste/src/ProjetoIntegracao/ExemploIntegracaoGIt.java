3d5aa747e7e626d54f33db7230189192f11cb9dc
TurmaMoreDevs <moredevs2blumenaujava@gmail.com>
2024-11-25 21:22:18 -0300

    segunda alteracao

--- Projetoteste/src/ProjetoIntegracao/ExemploIntegracaoGIt.java
+++ Projetoteste/src/ProjetoIntegracao/ExemploIntegracaoGIt.java
@@ -4,6 +4,7 @@ public class ExemploIntegracaoGIt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("teste");
+		System.out.println("segunda alteracao");
 
 	}
 
